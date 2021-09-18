320d9bfd7a170c3e19d884332dcd00df72b1baa2
Emo <emotupev11@gmail.com>
2021-09-18 22:38:51 +0300

    feat: add some strange functionality

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,21 +1,32 @@
-import EventEmitter from "eventemitter3";
+import EventEmitter from 'eventemitter3';
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+	static get events() {
+		return {
+			READY: 'ready',
+		};
+	}
 
-  constructor() {
-    super();
-    this.emojis = [];
-    this.banana = "🍌";
-    this.emit(Application.events.READY);
-  }
-  setEmojis(emojis) {
-    this.emojis = emojis;
-  }
+	constructor() {
+		super();
+		this.emojis = [];
+		this.banana = '🍌';
+		this.emit(Application.events.READY);
+	}
+	setEmojis(emojis) {
+		this.emojis = emojis;
 
-  addBananas() {}
+		this.addBananas();
+		const p = document.createElement('p');
+		p.textContent = this.emojis;
+
+		document.querySelector('#emojis').appendChild(p);
+		console.log(p.textContent);
+	}
+
+	addBananas() {
+		this.emojis = this.emojis.map((emoji) => {
+			return (emoji += this.banana);
+		});
+	}
 }
